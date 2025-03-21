0886a83ce91bbebe88af5ef157e38eaf5c0769d4
Diaz RobertJames <diazrj54@gmail.com>
2025-03-21 15:55:47 +0800

    Update main.py
    fix errors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+import json
+import os 
 
 #CREATIONAL, ABSTRACT-FACTORY
 class Account(ABC):
@@ -7,54 +9,87 @@ class Account(ABC):
     def __init__(self, name, password):
         self.name = name 
         self.password = password
-        self.user_num = Account.users + 1
-        Account.users = self.user_num
+        user_num = Account.users + 1
+        self.user_num = str(user_num).zfill(3)
+        Account.users += 1
+        
     
 class User(Account):
     def __init__(self, name, password):
         super().__init__(name, password)
+        
     
 class Admin(Account):
+    
     def __init__(self, name, password):
         super().__init__(name, password)
-        self.accounts = self.get_accts()  #fix not here
-    def create_account(self, type, name, password):
+        self.accounts = self.load_accts()
+        
+    def create_account(self, acc_type, name, password):
         creator = AccountFactory()
-        if type.lower() == 'user':
-            return creator.createUserAccount(name, password)
-        elif type.lower() == 'admin':
-            return creator.createAdminAccount(name, password)
+        acc_type = acc_type.lower()
+        if acc_type == 'user':
+            new_acc = creator.create_user_account(name, password)
+        elif acc_type == 'admin':
+            new_acc = creator.create_admin_account(name, password)
         else:
             return 'Please Input Valid Account Type'
+        
+        if acc_type not in self.accounts:
+            self.accounts[acc_type] = {}
+        
+        data = vars(new_acc).copy()
+        if 'accounts' in data:
+            del data['accounts']
+        
+        user_number = data.pop("user_num")
+        self.accounts[acc_type][user_number] = data
+        self.update_accts()
+        
+        return 'Account Succesfully Added'
+        
             
-    def get_accts(self): #fix not here
+    def load_accts(self): 
+        if not os.path.exists('accounts.json'):
+            return {}
+        
         with open('accounts.json', 'r') as file:
-            return json.load(file)
+            try:
+                return json.load(file)
+            except:
+                return {}
             
-    def update_accts(self): #fix not here should be in main system
+    def update_accts(self): 
         with open('accounts.json', 'w') as file:
             json.dump(self.accounts, file)
         return 'Succesfully Saved'
 
 class Factory(ABC):
+    
     @abstractmethod
-    def createUserAccount(self):
+    def create_user_account(self):
         pass
     
     @abstractmethod
-    def createAdminAccount(self):
+    def create_admin_account(self):
         pass
     
 class AccountFactory(Factory):
-    def createUserAccount(self, name, password):
+    def create_user_account(self, name, password):
         return User(name, password)
     
-    def createAdminAccount(self,name, password):
+    def create_admin_account(self,name, password):
         return Admin(name, password)
 
 
 if __name__ == "__main__":
     creator = AccountFactory()
-    admin1 = creator.createAdminAccount('rj', '123')
+    admin1 = creator.create_admin_account('rj', '123')
+
     
-    admin1.create_account('user', 'rj', '123')
+    print(admin1.create_account('user', 'rj', '123'))
+    print(admin1.create_account('user', 'rjf', '123'))
+    
+    
+    print(admin1.create_account('admin', 'Serg', '123'))
+    print(admin1.accounts)
